1a69a32f9b5b42fa5ed274517c326834a8d8acc3
SaadCodes <121240848+SaadiCodes@users.noreply.github.com>
2023-03-03 22:13:46 +0500

    initial wip

---
+++ src/index.js
@@ -0,0 +1,12 @@
+let div = document.querySelector("div#content")
+let h1 = document.createElement("h1")
+h1.textContent = "The Delight Restaurant"
+div.appendChild(h1)
+
+let h2 = document.createElement("h2")
+h2.textContent = "Our Timings"
+div.appendChild(h2)
+
+let h2two = document.createElement("h2")
+h2two.textContent = "Our Dishes"
+div.appendChild(h2two)
